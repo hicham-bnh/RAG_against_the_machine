6683732f46d0c7c2e41353999f9e7b4841041a3f
hicham-bnh <benhabibmohammedhicham@gmail.com>
2026-05-30 04:18:26 +0200

    searche finish

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,10 @@
 from src.chunker.chunker import Chunker
+from src.search.search import Search
 from src.indexer.indexer import Indexer
 
 test = Chunker()
-index = Indexer()
-index.indexing(test.chunk_data())
+indxer = Indexer()
+retriever, chunks = indxer.load()
+indxer.indexing(test.chunk_data())
+search = Search()
+result = search.search("how to add two function?", 10, retriever, chunks)
---
+++ src/search/search.py
@@ -0,0 +1,21 @@
+import bm25s
+from src.indexer.indexer import Indexer
+from src.validation.validation import MinimalSource
+
+
+class Search:
+    def __init__(self) -> None:
+        pass
+
+    def search(self, qst: str, k: int, retrieve, chunks):
+        result = []
+        qst_token = bm25s.tokenize([qst])
+        results, scores = retrieve.retrieve(qst_token, k=k)
+        for chunk_idx in results[0]:
+            chunk = chunks[chunk_idx]
+            result.append(MinimalSource(
+                file_path=chunk.file_path,
+                first_character_index=chunk.first_character_index,
+                last_character_index=chunk.last_character_index
+            ))
+        return result
